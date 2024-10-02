c45cff54ae40a3db118fb956e6dcfeecbba4e7fa
ajorgen3 <113084414+ajorgen3@users.noreply.github.com>
2024-10-01 23:19:16 -0400

    Structuring

--- campuscomfort/lib/main.dart
+++ campuscomfort/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:campuscomfort/map_sample.dart';
+import 'package:campuscomfort/map_tab.dart';
+import 'package:campuscomfort/my_reviews_tab.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -43,8 +45,8 @@ class MyHomePage extends StatelessWidget {
                       Tab(text: 'My Reviews'),
                     ])),
                 body: const TabBarView(children: [
-                  MapSample(),
-                  Center(child: Text('My Reviews')),
+                  MapTab(),
+                  MyReviewsTab(),
                 ]))));
   }
 }
---
+++ campuscomfort/lib/map_tab.dart
@@ -0,0 +1,13 @@
+import 'package:campuscomfort/map_sample.dart';
+import 'package:flutter/material.dart';
+
+class MapTab extends StatelessWidget {
+  const MapTab({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MapSample();
+  }
+}
